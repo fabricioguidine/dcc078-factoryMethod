dda0146ca150df5ebb40ca0d7aeb14dda2e42b70
fabricioguidine <fabricioguidine@gmail.com>
2023-09-17 20:58:20 -0300

    cria interface e classes de serviço

---
+++ src/main/java/org/example/ServicoCartao.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ServicoCartao implements IServico{
+
+    @Override
+    public String executar() {
+        return "Pagamento por Cartão de Cédito.";
+    }
+
+    @Override
+    public String cancelar() {
+        return "Pagamento cancelado.";
+    }
+}
---
+++ src/main/java/org/example/ServicoPix.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ServicoPix implements IServico{
+    @Override
+    public String executar() {
+        return "Pagamento por Pix.";
+    }
+
+    @Override
+    public String cancelar() {
+        return "Pagamento por Pix.";
+    }
+}
